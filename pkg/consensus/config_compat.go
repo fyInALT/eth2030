a297bf7a4604f2fc2bb754acaaa4c1e36e2e50b8
fyInALT <fy@altresear.ch>
2026-03-06 17:12:34 +0800

    refactor(consensus): split ConsensusConfig into sub-pkg

---
+++ pkg/consensus/config_compat.go
@@ -0,0 +1,12 @@
+package consensus
+
+// config_compat.go re-exports types from consensus/clconfig for backward compatibility.
+
+import "github.com/eth2030/eth2030/consensus/clconfig"
+
+// ConsensusConfig type alias.
+type ConsensusConfig = clconfig.ConsensusConfig
+
+// ConsensusConfig function wrappers.
+func DefaultConfig() *ConsensusConfig     { return clconfig.DefaultConfig() }
+func QuickSlotsConfig() *ConsensusConfig  { return clconfig.QuickSlotsConfig() }
