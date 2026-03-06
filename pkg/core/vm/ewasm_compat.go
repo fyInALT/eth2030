0307db5e88a7aa4286a922a4f2715b5634a77ed6
fyInALT <fy@altresear.ch>
2026-03-06 17:12:34 +0800

    refactor(vm): split ABI encoder into sub-pkg

--- pkg/core/vm/ewasm_compat.go
+++ pkg/core/vm/ewasm_compat.go
@@ -39,47 +39,47 @@ type (
 
 // Ewasm error variables.
 var (
-	ErrInterpStackOverflow    = ewasm.ErrInterpStackOverflow
-	ErrInterpStackUnderflow   = ewasm.ErrInterpStackUnderflow
-	ErrInterpDivisionByZero   = ewasm.ErrInterpDivisionByZero
-	ErrInterpOutOfGas         = ewasm.ErrInterpOutOfGas
-	ErrInterpUnknownOpcode    = ewasm.ErrInterpUnknownOpcode
-	ErrInterpLocalOutOfRange  = ewasm.ErrInterpLocalOutOfRange
-	ErrInterpUnreachable      = ewasm.ErrInterpUnreachable
-	ErrInterpInvalidSelect    = ewasm.ErrInterpInvalidSelect
-	ErrWASMStackOverflow      = ewasm.ErrWASMStackOverflow
-	ErrWASMStackUnderflow     = ewasm.ErrWASMStackUnderflow
-	ErrWASMOutOfMemory        = ewasm.ErrWASMOutOfMemory
-	ErrWASMDivisionByZero     = ewasm.ErrWASMDivisionByZero
-	ErrWASMUnreachable        = ewasm.ErrWASMUnreachable
+	ErrInterpStackOverflow     = ewasm.ErrInterpStackOverflow
+	ErrInterpStackUnderflow    = ewasm.ErrInterpStackUnderflow
+	ErrInterpDivisionByZero    = ewasm.ErrInterpDivisionByZero
+	ErrInterpOutOfGas          = ewasm.ErrInterpOutOfGas
+	ErrInterpUnknownOpcode     = ewasm.ErrInterpUnknownOpcode
+	ErrInterpLocalOutOfRange   = ewasm.ErrInterpLocalOutOfRange
+	ErrInterpUnreachable       = ewasm.ErrInterpUnreachable
+	ErrInterpInvalidSelect     = ewasm.ErrInterpInvalidSelect
+	ErrWASMStackOverflow       = ewasm.ErrWASMStackOverflow
+	ErrWASMStackUnderflow      = ewasm.ErrWASMStackUnderflow
+	ErrWASMOutOfMemory         = ewasm.ErrWASMOutOfMemory
+	ErrWASMDivisionByZero      = ewasm.ErrWASMDivisionByZero
+	ErrWASMUnreachable         = ewasm.ErrWASMUnreachable
 	ErrEWASMPrecompileNotFound = ewasm.ErrEWASMPrecompileNotFound
-	ErrEWASMExecutionFailed   = ewasm.ErrEWASMExecutionFailed
-	ErrEWASMOutOfGas          = ewasm.ErrEWASMOutOfGas
-	ErrEWASMAlreadyRegistered = ewasm.ErrEWASMAlreadyRegistered
+	ErrEWASMExecutionFailed    = ewasm.ErrEWASMExecutionFailed
+	ErrEWASMOutOfGas           = ewasm.ErrEWASMOutOfGas
+	ErrEWASMAlreadyRegistered  = ewasm.ErrEWASMAlreadyRegistered
 )
 
 // Ewasm function wrappers.
-func NewEWASMEngine() *EWASMEngine                { return ewasm.NewEWASMEngine() }
+func NewEWASMEngine() *EWASMEngine { return ewasm.NewEWASMEngine() }
 func NewWASMExecutor(config WASMExecutorConfig) *WASMExecutor {
 	return ewasm.NewWASMExecutor(config)
 }
 func NewEWASMInterpreter(config EWASMInterpreterConfig) *EWASMInterpreter {
 	return ewasm.NewEWASMInterpreter(config)
 }
-func NewI32Const(val uint32) WasmOp               { return ewasm.NewI32Const(val) }
-func NewI64Const(val uint64) WasmOp               { return ewasm.NewI64Const(val) }
-func NewLocalGet(idx uint32) WasmOp               { return ewasm.NewLocalGet(idx) }
-func NewLocalSet(idx uint32) WasmOp               { return ewasm.NewLocalSet(idx) }
-func NewCallOp(funcIdx uint32) WasmOp             { return ewasm.NewCallOp(funcIdx) }
+func NewI32Const(val uint32) WasmOp   { return ewasm.NewI32Const(val) }
+func NewI64Const(val uint64) WasmOp   { return ewasm.NewI64Const(val) }
+func NewLocalGet(idx uint32) WasmOp   { return ewasm.NewLocalGet(idx) }
+func NewLocalSet(idx uint32) WasmOp   { return ewasm.NewLocalSet(idx) }
+func NewCallOp(funcIdx uint32) WasmOp { return ewasm.NewCallOp(funcIdx) }
 func NewOptimizationPipeline(passes ...OptimizationPass) *OptimizationPipeline {
 	return ewasm.NewOptimizationPipeline(passes...)
 }
 func NewEWASMPrecompileRegistry() *EWASMPrecompileRegistry {
 	return ewasm.NewEWASMPrecompileRegistry()
 }
-func NewJITCache(capacity int) *JITCache         { return ewasm.NewJITCache(capacity) }
-func DefaultWasmGasCalculator() *WasmGasCalculator { return ewasm.DefaultWasmGasCalculator() }
-func ValidateWasmBytecode(code []byte) error     { return ewasm.ValidateWasmBytecode(code) }
+func NewJITCache(capacity int) *JITCache             { return ewasm.NewJITCache(capacity) }
+func DefaultWasmGasCalculator() *WasmGasCalculator   { return ewasm.DefaultWasmGasCalculator() }
+func ValidateWasmBytecode(code []byte) error         { return ewasm.ValidateWasmBytecode(code) }
 func CompileModule(code []byte) (*WasmModule, error) { return ewasm.CompileModule(code) }
 func ExecuteExport(module *WasmModule, funcName string, args []byte) ([]byte, error) {
 	return ewasm.ExecuteExport(module, funcName, args)
